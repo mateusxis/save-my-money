19a7c2f7a0c81c5011f8f0f676fb354c01114844
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-05-04 10:15:29 -0300

    [database] refactor add uri

--- src/database.js
+++ src/database.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const { user, password, database } = require("./config");
 
-mongoose.connect(
-  `mongodb+srv://${user}:${password}@clusterxis.6owt6.mongodb.net/${database}?retryWrites=true&w=majority`,
+const mongoURI = `mongodb://${user}:${password}@clusterxis.6owt6.mongodb.net/${database}?retryWrites=true&w=majority`;
+
+module.exports = () => mongoose.connect(
+  mongoURI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
-module.exports = () => mongoose;
