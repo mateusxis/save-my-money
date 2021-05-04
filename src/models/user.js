1fccd38d02de740a66f2fe263a1d28db14e04a5d
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-05-04 10:14:27 -0300

    [user] Feat create model

---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    nickname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", UserSchema);
