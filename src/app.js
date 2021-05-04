59cefb8b6b3fe7362df516354c4ead713a1c6bfa
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-05-04 10:15:55 -0300

    [app] feat add routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
-const Koa =  require('koa');
+const Koa = require("koa");
+const mongoose = require("mongoose");
 
 const middlewares = require('./middlewares');
 const database = require('./database');
+const routesFactory = require('./routes');
+
 const { port } = require('./config');
 
 const app = new Koa();
+const routes = routesFactory();
+
 
 const configServer = async () => {
   middlewares(app);
+  app
+  .use(routes.routes())
+  .use(routes.allowedMethods());
   database();
 };
 
 const startServer = async () => {
-  app.listen(port,  () => console.log(`Listening on port ${port}`));
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 };
 
 module.exports = {
   init: async () => {
     configServer();
     startServer();
-  }
-}
+  },
+};
